baec4691f92e000e5d99504758f5d874e7ad8179
Bhavya Shah <62424200+BhavyaShah1234@users.noreply.github.com>
2020-09-08 22:28:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function get_rem_time(endtime)
+{
+    var t = Date.parse(endtime) - Date.parse(new Date())
+    var seconds = Math.floor((t / 1000) % 60)
+    var minutes = Math.floor((t / 1000/ 60) % 60)
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24)
+    var days = Math.floor(t / (1000 * 60 * 60 * 24))
+    return {'total': t, 'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds}
+}
+function initialize_clock(id, endtime)
+{
+    var clock = document.getElementById(id)
+    var daysSpan = clock.querySelector('.days')
+    var hoursSpan = clock.querySelector('.hours')
+    var minutesSpan = clock.querySelector('.minutes')
+    var secondsSpan = clock.querySelector('.seconds')
+
+    function update_clock()
+    {
+        var t = get_rem_time(endtime)
+        daysSpan.innerHTML = t.days
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2)
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2)
+        secondsSpan.innerHTML = ('0'+ t.seconds).slice(-2)
+
+        if(t.total <= 0)
+        {
+            clearInterval(timeinterval)
+        }
+    }
+    update_clock()
+    var timeinterval = setInterval(update_clock, 1000)
+}
+var deadline = new Date(Date.parse(new Date()) + 7 * 24 * 60 * 60 * 1000)
+initialize_clock('clock', deadline)
